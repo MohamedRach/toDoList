39c88b4a0542ba8187cfea11dfd044acdefde74e
MohamedRach <rachidi.mohamed.sadek@gmail.com>
2023-02-15 14:17:42 +0100

    adding a little bit of styling

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 tasks = document.querySelector(".tasks");
-button = document.getElementById("add");
+button = document.querySelector(".addTask");
 input = document.getElementById("input");
-
+container = document.querySelector(".container");
+checkbox = document.getElementById("checkbox");
+indiTask = document.querySelectorAll(".indiTask");
 button.addEventListener('click', e => {
     e.preventDefault();
     let task = input.value;
-    const para = document.createElement("p");
-    const node = document.createTextNode(task);
-    para.appendChild(node);
-    tasks.appendChild(para);
+    tasks.innerHTML += `<div class="task"> <input type="checkbox" name="checkbox" id="checkbox"> <p class="indiTask">${task}</p></div>`
     input.value = "";
+    indiTask = document.querySelectorAll(".indiTask");
+    indiTask.forEach(indi => {
+      let checkbox1 = indi.previousElementSibling;
+      checkbox1.addEventListener("click", e => {
+        if(indi.classList.contains("check")){
+          indi.classList.remove("check");
+        } else {
+          indi.classList.add("check");
+        }
+      })
+    })
+
 })
+
 input.addEventListener("keypress", function(event) {
     // If the user presses the "Enter" key on the keyboard
     if (event.key === "Enter") {
@@ -18,10 +30,28 @@ input.addEventListener("keypress", function(event) {
         event.preventDefault();
       // Trigger the button element with a click
         let task = input.value;
-        const para = document.createElement("p");
-        const node = document.createTextNode(task);
-        para.appendChild(node);
-        tasks.appendChild(para);
+        tasks.innerHTML += `<div class="task"> <input type="checkbox" name="checkbox" id="checkbox"> <p class="indiTask">${task}</p></div>`
         input.value = "";
+        indiTask = document.querySelectorAll(".indiTask");
+        indiTask.forEach(indi => {
+          let checkbox1 = indi.previousElementSibling;
+          checkbox1.addEventListener("click", e => {
+            if(indi.classList.contains("check")){
+              indi.classList.remove("check");
+            } else {
+              indi.classList.add("check");
+            }
+          })
+        })
     }
   });
+indiTask.forEach(indi => {
+    let checkbox1 = indi.previousElementSibling;
+    checkbox1.addEventListener("click", e => {
+      if(indi.classList.contains("check")){
+        indi.classList.remove("check");
+      } else {
+        indi.classList.add("check");
+      }
+    })
+  })
